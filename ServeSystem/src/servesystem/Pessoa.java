ab5f5346061c16f6772bf40d83f14dfd8f0ac070
ridethejustce <lucascarvalho1995@hotmail.com>
2014-05-12 23:33:15 -0300

    comecinho
    criei umas classes basicas sem implementar nada

---
+++ ServeSystem/src/servesystem/Pessoa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servesystem;
+
+/**
+ *
+ * @author User
+ */
+public class Pessoa {
+    private String nome;
+    private String telefone;
+    Pessoa(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+    
+}
